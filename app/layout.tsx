a05d5580b33ef1fb21613c3dae04e2732141223a
Akinwunmi Femi <feezyakinwunmi001@gmail.com>
2026-03-25 15:07:04 -0700

    bg change

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark scroll-smooth">
       <Navbar/>
-      <body className="bg-black text-white overflow-x-hidden">{children}</body>
+      <body className="bg-white text-white overflow-x-hidden">{children}</body>
       <Footer/>
     </html>
   );
