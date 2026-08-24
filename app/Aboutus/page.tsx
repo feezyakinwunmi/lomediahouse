f1f5d83db0743d7731b3c546780c3d94f2659721
Ajigbotoluwa <covenanttofunmi2@gmail.com>
2026-08-24 10:52:49 +0100

    about image

--- app/Aboutus/page.tsx
+++ app/Aboutus/page.tsx
@@ -372,7 +372,7 @@ export default function AboutPage() {
                   <img 
                     src={img}
                     alt="Team member"
-                    className="w-[90%] h-full object-fit rounded-2xl hover:scale-110 transition-transform duration-500"
+                    className="w-[80%]  h-full object-contain rounded-2xl hover:scale-110 transition-transform duration-500"
                   />
                 </div>
               ))}
